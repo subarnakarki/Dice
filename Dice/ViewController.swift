27bee1b46c2f059f184e362e8388055edc8e2a8d
Subarna Karki <subarna.karki13@gmail.com>
2020-01-24 19:06:05 -0500

    Initial Commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by subarna karki on 1/24/20.
+//  Copyright © 2020 Subarna Karki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
