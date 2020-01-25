b84976b9643f3e9e4ef40f07df51a9248652cef2
Subarna Karki <subarna.karki13@gmail.com>
2020-01-24 22:53:38 -0500

    Create rollDice and updateDiceImages functions

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0;
+    var randomDiceIndex2 : Int = 0;
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -19,7 +24,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
+        updateDiceImages()
+    }
+    
+    func updateDiceImages () {
+    diceImageView1.image =  UIImage(named: diceArray[rollDice()])
+    diceImageView2.image =  UIImage(named: diceArray[rollDice()])
+    }
+    
+    func rollDice() -> Int {
+        let diceNumber = Int.random(in: 0 ..< 6)
+        return diceNumber
     }
     
 }
